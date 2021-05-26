0495b6645883e9fb8acf9fa609238c02c6c42a02
LaertNuhu <laertnuhu@gmail.com>
2021-05-26 12:25:45 +0200

    [add] initial scripts

---
+++ benchmark/benchmark.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import jinja2
+import prestodb.dbapi as presto
+from pydrill.client import PyDrill
+import pandas as pd
+import time
+
+class Benchmark()
+
+    def __init__(self):
+        self.m = ""
---
+++ benchmark/configurator.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import jinja2
+import prestodb.dbapi as presto
+from pydrill.client import PyDrill
+import pandas as pd
+import time
+
+class Configurator()
+
+    def __init__(self):
+        self.m = ""
+
+    def get_source()
