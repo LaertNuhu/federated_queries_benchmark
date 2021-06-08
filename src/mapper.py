bcde47351144817955583f415352722b1ba32b21
LaertNuhu <laertnuhu@gmail.com>
2021-06-08 20:43:43 +0200

    [added] tables schema and changed tables name to long version

--- src/mapper.py
+++ src/mapper.py
@@ -1,46 +1,44 @@
 class Mapper:
-    def map_tables_to_sources(self, scale, **kwargs):
+    def map_tables_torderss(self, scale, **kwargs):
         """Returns a dict with tabels maped to sources"""
         result = {}
         missing_tables = []
         result.update(
             {
-                "lineitem": f"{kwargs['li_source']}.public.{kwargs['li_source'][:-1]}_{scale}_lineitem"
+                "lineitem": f"{kwargs['lineitem']}.public.{kwargs['lineitem'][:-1]}_{scale}_lineitem"
             }
-        ) if "li_source" in kwargs else missing_tables.append("lineitem")
+        ) if "lineitem" in kwargs else missing_tables.append("lineitem")
         result.update(
             {
-                "orders": f"{kwargs['o_source']}.public.{kwargs['o_source'][:-1]}_{scale}_orders"
+                "orders": f"{kwargs['orders']}.public.{kwargs['orders'][:-1]}_{scale}_orders"
             }
-        ) if "o_source" in kwargs else missing_tables.append("orders")
+        ) if "orders" in kwargs else missing_tables.append("orders")
         result.update(
             {
-                "customer": f"{kwargs['c_source']}.public.{kwargs['c_source'][:-1]}_{scale}_customer"
+                "customer": f"{kwargs['customer']}.public.{kwargs['customer'][:-1]}_{scale}_customer"
             }
-        ) if "c_source" in kwargs else missing_tables.append("customer")
+        ) if "customer" in kwargs else missing_tables.append("customer")
+        result.update(
+            {"part": f"{kwargs['part']}.public.{kwargs['part'][:-1]}_{scale}_part"}
+        ) if "part" in kwargs else missing_tables.append("part")
         result.update(
             {
-                "part": f"{kwargs['pa_source']}.public.{kwargs['pa_source'][:-1]}_{scale}_part"
+                "supplier": f"{kwargs['supplier']}.public.{kwargs['supplier'][:-1]}_{scale}_supplier"
             }
-        ) if "pa_source" in kwargs else missing_tables.append("part")
+        ) if "supplier" in kwargs else missing_tables.append("supplier")
         result.update(
             {
-                "supplier": f"{kwargs['s_source']}.public.{kwargs['s_source'][:-1]}_{scale}_supplier"
+                "partsupp": f"{kwargs['partsupp']}.public.{kwargs['partsupp'][:-1]}_{scale}_partsupp"
             }
-        ) if "s_source" in kwargs else missing_tables.append("supplier")
+        ) if "partsupp" in kwargs else missing_tables.append("partsupp")
         result.update(
             {
-                "partsupp": f"{kwargs['ps_source']}.public.{kwargs['ps_source'][:-1]}_{scale}_partsupp"
+                "nation": f"{kwargs['nation']}.public.{kwargs['nation'][:-1]}_{scale}_nation"
             }
-        ) if "ps_source" in kwargs else missing_tables.append("partsupp")
+        ) if "nation" in kwargs else missing_tables.append("nation")
         result.update(
             {
-                "nation": f"{kwargs['n_source']}.public.{kwargs['n_source'][:-1]}_{scale}_nation"
+                "region": f"{kwargs['region']}.public.{kwargs['region'][:-1]}_{scale}_region"
             }
-        ) if "n_source" in kwargs else missing_tables.append("nation")
-        result.update(
-            {
-                "region": f"{kwargs['r_source']}.public.{kwargs['r_source'][:-1]}_{scale}_region"
-            }
-        ) if "r_source" in kwargs else missing_tables.append("region")
+        ) if "region" in kwargs else missing_tables.append("region")
         return result
